a23e5437c57fc0acafb4eb24351b72c75d735373
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-10-11 01:14:57 -0400

    added commetns to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,29 @@
+//adds full length day of the week, month, and ordinal date to jumbotron
 jumbotronDate = moment().format('dddd, MMMM Do');
 $('#currentDay').text(jumbotronDate);
 
-$(document).ready(function() {
-    $('.saveBtn').on('click', function(){
+
+$(document).ready(function() { //ensures fucntion will only run after DOM is ready
+    $('.saveBtn').on('click', function(){ //adds event listener on save button for 'click'
+        //gets nearby values and stores them in variables
         var text = $(this).siblings('.description').val();
         var time = $(this).parent().attr('id');
 
+        //sets the variables above as items in local storage
         localStorage.setItem(time, text);
     })
 
-    function timeColorTracker() {
+    
+    function timeColorTracker() {//function for color-coding each hour as past, present, or future live on the site
+        //stores real-time from moment.js in variable
         var currentHour = moment().hour();
 
+        //for loop for each time block div 
         $('.time-block').each(function () {
-            var blockHour = parseInt($(this).attr('id').split('hour')[1]);
+            var blockHour = parseInt($(this).attr('id').split('hour')[1]); //takes only the number after hour as variable
+            //console.log(blockHour)
 
+            //checks hours in each block to current hours and changes color by adding or removing css via class
             if (blockHour < currentHour) {
                 $(this).addClass('past')
                 $(this).removeClass('present')
@@ -31,6 +40,8 @@ $(document).ready(function() {
         })
     }
 
+
+    //loads saved data from local storage if there is any for each hour block
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
@@ -41,5 +52,5 @@ $(document).ready(function() {
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-   timeColorTracker();
+   timeColorTracker(); //runs color changing function again 
 })
